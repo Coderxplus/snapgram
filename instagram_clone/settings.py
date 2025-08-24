84ab29b534e43dbd150743fd62889f6569907baf
unknown <okereebube87@gmail.com>
2025-08-24 11:51:35 +0100

    ...

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -142,7 +142,7 @@ CLOUDINARY_URL=os.environ.get("CLOUDINARY_URL")
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get("CLOUDINARY_CLOUD_NAME"),
     'API_KEY': os.environ.get("CLOUDINARY_API_KEY"),
-    'API_SECRET': os.environ.get("CLOUDINARY_API_SECRET ")
+    'API_SECRET': os.environ.get("CLOUDINARY_API_SECRET")
 }
 
 # Default primary key field type
